e9624cfac30360b4fd6bd4b5376cf70ebd607fc0
a1den662 <aidenjmark588@gmail.com>
2025-07-03 14:02:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/home.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/about', (req, res) => {

d30703d8a21f7c09c14229c6b53c1f665fbb4551
a1den662 <aidenjmark588@gmail.com>
2025-07-03 13:51:48 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { run } = require('node:test');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+  });
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/contact.html");
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
